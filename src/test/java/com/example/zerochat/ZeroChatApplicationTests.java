a77469dc7ee21d15382dc4d97313f7c07dd3dfe5
inzero <inzero817@gmail.com>
2024-06-20 18:11:53 +0900

    :bulb: 프론트 + 백 빌드

---
+++ src/test/java/com/example/zerochat/ZeroChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zerochat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
